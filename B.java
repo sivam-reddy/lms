80c102d7670981ce1b0dd2185a500dfdf1018c90
sivaprasadm317@gmail.com <sivaprasadm317@gmail.com>
2023-09-21 09:51:29 +0530

    message

---
+++ B.java
@@ -0,0 +1,20 @@
+class B 
+{
+	static  byte i;
+	static  short j;
+	static  int k;
+	static  long l;
+	static  float o;
+	static  double m;
+	static  boolean n;
+	public static void main(String[] args) 
+	{
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(l);
+		System.out.println(m);
+		System.out.println(n);
+		System.out.println(o);
+	}
+}
